246dc51b0061a098b96a60bd40de5c340e436f74
carbon-ahs <shehanuk.ahsan@gmail.com>
2024-03-06 17:47:16 +0600

    item data class added

---
+++ app/src/main/java/com/example/recyclerviewintro/data/Item.java
@@ -0,0 +1,27 @@
+package com.example.recyclerviewintro.data;
+
+public class Item {
+    private String name;
+    private String phoneNumber;
+
+    public Item(String name, String phoneNumber) {
+        this.name = name;
+        this.phoneNumber = phoneNumber;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+}
